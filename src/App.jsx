952751e2c76f679b45dceb951054fdb3a38e678b
AdonsFCO <adonsfco@gmail.com>
2022-12-23 00:20:24 -0400

    [Fix] Remove header from the main container

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import ArticleComponent from "./components/Article/Article";
 import Header from "./components/Header/Header";
 
 class App extends Component {
@@ -7,9 +8,19 @@ class App extends Component {
   }
   render() {
     return (
+      <>
+       <Header />
       <div className="container">
-        <Header />
+       
+        <ArticleComponent title="Bussines Name Or Tagline">
+          Do ex proident deserunt nisi.Id dolor consectetur cupidatat
+          exercitation reprehenderit pariatur. Deserunt incididunt aute ad id
+          aliquip do Lorem proident culpa pariatur laborum culpa officia
+          commodo. Ea excepteur et officia ullamco deserunt anim occaecat
+          excepteur cupidatat aute commodo velit deserunt anim.
+        </ArticleComponent>
       </div>
+      </>
     );
   }
 }
